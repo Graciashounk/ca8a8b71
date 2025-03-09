95d5b8f1843aa94f3722136ad60dc9fbbf3b6ea4
Graciashounk <donahounkanrin@gmail.com>
2025-03-08 23:06:21 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { config } from 'dotenv'
+
+// Load environment variables
+config()
 
 // https://vite.dev/config/
 export default defineConfig({
